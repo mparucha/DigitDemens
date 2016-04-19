82cb829586968c7102e3afec2f4815a44d50d82e
Marek <Marek@Marek-PC.local>
2016-04-19 21:29:45 +0200

    laptop

---
+++ DigitDemens/src/de/hhn/aib/digitdemens/view/DigitDemensView.java
@@ -0,0 +1,21 @@
+package de.hhn.aib.digitdemens.view;
+
+import javax.swing.JFrame;
+
+public class DigitDemensView extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DigitDemensView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
